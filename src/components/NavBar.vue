a903c132691a0146a516cd633327e7fd93a0e093
Nguyễn Quốc Hợp <hop35889@gmail.com>
2024-03-14 23:34:27 +0700

    3.3 - new post Router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-menu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <RouterLink to="/posts/new" class="button"
+                            >New Post</RouterLink
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
