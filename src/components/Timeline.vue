350b44105b86815a42ed82140f6c7345007994e6
Nguyễn Quốc Hợp <hop35889@gmail.com>
2024-03-14 09:38:15 +0700

    1.8 - events; fix path not recognized

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 const periods = ["Today", "This Week", "This Month"];
+const selectedPeriod = ref(periods[0]);
+
+function selectPeriod(period: string) {
+    selectedPeriod.value = period;
+}
 </script>
 
 <template>
     <nav class="panel">
+        {{ selectedPeriod }}
         <div class="panel-tabs">
-            <a v-for="period in periods" :key="period"> {{ period }}</a>
+            <a
+                v-for="period in periods"
+                :key="period"
+                v-on:click="selectPeriod(period)"
+            >
+                {{ period }}</a
+            >
         </div>
     </nav>
 </template>
