394b02dbd15515d398808b95a7f87a0889661157
Nguyễn Quốc Hợp <hop35889@gmail.com>
2024-03-14 10:10:14 +0700

    1.12 - DateTimes with Luxon

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
+import { Post, today, thisMonth, thisWeek } from "../posts";
+import { DateTime } from "luxon";
+
 const periods = ["Today", "This Week", "This Month"] as const;
 type Period = (typeof periods)[number];
 
 const selectedPeriod = ref<Period>(periods[0]);
+
+const posts = [today, thisWeek, thisMonth].map((post) => {
+    return {
+        ...post,
+        created: DateTime.fromISO(post.created),
+    };
+});
 
 function selectPeriod(period: Period) {
     selectedPeriod.value = period;
@@ -24,5 +34,10 @@ function selectPeriod(period: Period) {
                 {{ period }}</a
             >
         </div>
+
+        <a v-for="post of posts" :key="post.id" class="panel-block">
+            <a> {{ post.title }} </a>
+            <div>{{ post.created.toFormat("d MMM") }}</div>
+        </a>
     </nav>
 </template>
