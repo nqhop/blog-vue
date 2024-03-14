de0c4a7e12255501efe33cf5ee3d6ae9ec971dd0
Nguyễn Quốc Hợp <hop35889@gmail.com>
2024-03-14 23:46:20 +0700

    3.3 - fix bug: createMemoryHistory

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,11 @@
 import { createRouter, createMemoryHistory } from "vue-router";
 import Home from "./views/Home.vue";
+import NewPost from "./views/NewPost.vue";
 
 export const router = createRouter({
     history: createMemoryHistory(),
-    routes: [{ path: "/", component: Home }],
+    routes: [
+        { path: "/", component: Home },
+        { path: "/posts/new", component: NewPost },
+    ],
 });
---
+++ src/views/NewPost.vue
@@ -0,0 +1,3 @@
+<template>
+    New post
+</template>
