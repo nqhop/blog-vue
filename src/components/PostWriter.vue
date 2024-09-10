9e61be5e641ec3a50f2a57975fbead61c53dd977
Nguyễn Quốc Hợp <hop35889@gmail.com>
2024-09-10 14:08:46 +0700

    3.10 - syntax highlighting and debounce

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -2,6 +2,9 @@
 import { onMounted, ref, watch, watchEffect } from "vue";
 import { TimelinePost } from "../posts";
 import { marked } from "marked";
+import highlightjs from "highlight.js";
+import { debounce } from "lodash";
+
 const props = defineProps<{
     post: TimelinePost;
 }>();
@@ -11,22 +14,46 @@ const content = ref(props.post.markdown);
 const html = ref("");
 const contenteditable = ref<HTMLDivElement>();
 
-watchEffect(() => {
-    marked.parse(content.value, (err, parseResult) => {
-        html.value = parseResult;
-    });
-});
-// watch(
-//     content,
-//     (newContent) => {
-//         marked.parse(newContent, (err, parseResult) => {
+// watchEffect(() => {
+//     marked.parse(
+//         content.value,
+//         {
+//             gfm: true,
+//             breaks: true,
+//             highlight: (code) => {
+//                 return highlightjs.highlightAuto(code).value;
+//             },
+//         },
+//         (err, parseResult) => {
 //             html.value = parseResult;
-//         });
-//     },
-//     {
-//         immediate: true,
-//     }
-// );
+//         }
+//     );
+// });
+
+function parseHtml(markdown: string) {
+    marked.parse(
+        markdown,
+        {
+            gfm: true,
+            breaks: true,
+            highlight: (code) => {
+                return highlightjs.highlightAuto(code).value;
+            },
+        },
+        (err, parseResult) => {
+            html.value = parseResult;
+        }
+    );
+}
+watch(
+    content,
+    debounce((newContent) => {
+        parseHtml(newContent);
+    }, 250),
+    {
+        immediate: true,
+    }
+);
 
 function handleInput() {
     if (!contenteditable.value) {
